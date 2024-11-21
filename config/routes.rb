0808893a560df783b236cee1718393d10befe0cb
Jeter Chen <dali1756@gmail.com>
2024-11-21 20:06:52 +0800

    feature:會員系統CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get 'home/index'
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  resources :users, only: [:create, :edit, :update, :destroy] do
+    collection do
+      get :sign_up
+      get :sign_in
+      post :login
+      delete :logout
+    end
+  end
+  get '/users', to: 'users#sign_up'
+  root "home#index"
 end
